d538b6992abe962b0a9c440e27f4dfb91302901a
M Iqbal Alamsyah S <ialamsyah92@gmail.com>
2024-09-10 21:17:04 +0700

    update env example

--- resources/js/Components/NotificationButton.tsx
+++ resources/js/Components/NotificationButton.tsx
@@ -54,12 +54,6 @@ function NotificationButton({ userId }: { userId: number }) {
         router.post(route('readNotif'), { id: data.id, ticket_id: data.ticket_id })
     };
 
-    // useEffect(() => {
-    //     if (showPopup) {
-
-    //     }
-    // }, [showPopup]);
-
     return (
         <div className="relative">
             <button onClick={handleButtonClick} type="button" className="relative inline-flex items-center p-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
